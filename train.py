c54d6733c64d43fbcaff09410e8c178d50a6b92c
Cuong Nguyen <cuong.vn08@gmail.com>
2017-10-09 21:31:20 +0900

    * Added training data 2016 and 2017. * Updated training result.

--- train.py
+++ train.py
@@ -19,19 +19,27 @@ warnings.warn = ignore_warn #ignore annoying warning (from sklearn and seaborn)
 
 ## STEP0: do setting
 class Settings(Enum):
-    global train_path
-    global properties_path
+    global train06_path
+    global train07_path
+    global properties06_path
+    global properties07_path
     global submission_path
     global XGB_WEIGHT
     global LGB_WEIGHT
-    global TRAIN_FULL_SET
+    global USE_VALID_DATA
+    global VALID_DATA_RATIO
+    global TUNE_PARAMS
     
-    train_path      = 'C:/data/kaggle/zillow_price/train_2016_v2.csv'
-    properties_path = 'C:/data/kaggle/zillow_price/properties_2016.csv'
-    submission_path = 'C:/data/kaggle/zillow_price/sample_submission.csv'
-    XGB_WEIGHT = 0.5
-    LGB_WEIGHT = 1 - XGB_WEIGHT
-    TRAIN_FULL_SET = True
+    train06_path      = 'C:/data/kaggle/zillow_price/train_2016_v2.csv'
+    train07_path      = 'C:/data/kaggle/zillow_price/train_2017.csv'
+    properties06_path = 'C:/data/kaggle/zillow_price/properties_2016.csv'
+    properties07_path = 'C:/data/kaggle/zillow_price/properties_2017.csv'
+    submission_path   = 'C:/data/kaggle/zillow_price/sample_submission.csv'
+    XGB_WEIGHT        = 0.5
+    LGB_WEIGHT        = 1 - XGB_WEIGHT
+    USE_VALID_DATA    = False
+    VALID_DATA_RATIO  = 0.2
+    TUNE_PARAMS       = False
     
     def __str__(self):
         return self.value
@@ -42,7 +50,7 @@ def analyze(df):
     print('\nAnalyzing ...')
     
     # show logerror
-    if(0):
+    if(1):
         plt.figure(figsize=(8,6))
         plt.scatter(range(df.shape[0]), np.sort(df.logerror.values))
         plt.xlabel('index', fontsize=12)
@@ -50,7 +58,7 @@ def analyze(df):
         plt.show()
         
     # show other features in correlation with logerror
-    if(0):
+    if(1):
         for feature in df:
             if df[feature].dtype == "float64" or "int64":
                 if feature != 'transactiondate':
@@ -61,7 +69,7 @@ def analyze(df):
                     plt.show()  
                     
     # show correlation
-    if(0):
+    if(1):
         xgb_params = {
             'eta': 0.05,
             'max_depth': 8,
@@ -140,112 +148,115 @@ def add_features(prop_df):
     
     medlong = prop_df.groupby('regionidneighborhood')['longitude'].aggregate('median').to_dict()
     prop_df['med_long'] = prop_df['regionidneighborhood'].map(medlong)
-
-    #prop_df['zip_std'] = prop_df['regionidzip'].map(zipstd)
-    #prop_df['city_std'] = prop_df['regionidcity'].map(citystd)
-    #prop_df['hood_std'] = prop_df['regionidneighborhood'].map(hoodstd)
     
-def remove_outliers(df):        
+def remove_outliers(df):
     print('\nRemoving outliers ...')
 
     df.drop(df[df['logerror'] > 0.419].index, axis=0, inplace=True)
     df.drop(df[df['logerror'] <-0.4].index, axis=0, inplace=True)
     
-    df.drop(df[(df['finishedsquarefeet15']>20000) & (df['logerror']<1)].index, axis=0, inplace=True)
-    df.drop(df[(df['garagecarcnt']>20) & (df['logerror']<0)].index, axis=0, inplace=True)
-    df.drop(df[(df['garagetotalsqft']>6000) & (df['logerror']<0)].index, axis=0, inplace=True)
-    df.drop(df[(df['lotsizesquarefeet']>6000000) & (df['logerror']<2)].index, axis=0, inplace=True)
-    df.drop(df[(df['poolsizesum']>1500) & (df['logerror']<2)].index, axis=0, inplace=True)
-    df.drop(df[(df['unitcnt']>60) & (df['logerror']<2)].index, axis=0, inplace=True)
-    df.drop(df[(df['taxdelinquencyyear']>80) & (df['logerror']<2)].index, axis=0, inplace=True)
-           
+    #df.drop(df[(df['finishedsquarefeet15']>20000) & (df['logerror']<1)].index, axis=0, inplace=True)
+    #df.drop(df[(df['garagecarcnt']>20) & (df['logerror']<0)].index, axis=0, inplace=True)
+    #df.drop(df[(df['garagetotalsqft']>6000) & (df['logerror']<0)].index, axis=0, inplace=True)
+    #df.drop(df[(df['lotsizesquarefeet']>6000000) & (df['logerror']<2)].index, axis=0, inplace=True)
+    #df.drop(df[(df['poolsizesum']>1500) & (df['logerror']<2)].index, axis=0, inplace=True)
+    #df.drop(df[(df['unitcnt']>60) & (df['logerror']<2)].index, axis=0, inplace=True)
+    #df.drop(df[(df['taxdelinquencyyear']>80) & (df['logerror']<2)].index, axis=0, inplace=True)
+   
 def select_features(df):
     print('\nSelecting important features ...')
     
-    #drop_features = ['parcelid', 'transactiondate',
-    #                 'airconditioningtypeid', 'buildingclasstypeid',
-    #                 'buildingqualitytypeid', 'regionidcity']
-    #drop_features = ['parcelid', 'transactiondate',
-    #                 'poolsizesum', 'storytypeid', 'typeconstructiontypeid',
-    #                 'decktypeid', 'pooltypeid10',
-    #                 ]
     drop_features = ['parcelid', 'transactiondate',
                      'regionidcounty', 
                      'poolsizesum', 
                      'yardbuildingsqft26', 
                      'decktypeid', 
                      'storytypeid', 
-                     'pooltypeid2', 
+                     'pooltypeid2',
                      ]
     
     df.drop(drop_features, axis=1, inplace=True)
 
 def _process_data():
     print('\n\nSTEP1: processing data ...')
-
-    global full_train_x
-    global full_train_y    
+        
     global train_x
     global train_y
     global valid_x
     global valid_y
     global test_x
+    
+    valid_x = None
+    valid_y = None
         
     # load data
     print('\nLoading data ...')
-    train_df = pd.read_csv(train_path, parse_dates=["transactiondate"])
-    prop_df = pd.read_csv(properties_path)
-    
+    train06_df = pd.read_csv(train06_path, parse_dates=["transactiondate"])
+    train07_df = pd.read_csv(train07_path, parse_dates=["transactiondate"])
+        
+    prop06_df = pd.read_csv(properties06_path, low_memory = False)
+    prop07_df = pd.read_csv(properties07_path, low_memory = False)
+        
     # fill NA
-    fill_NA(prop_df)
+    fill_NA(prop06_df)
+    fill_NA(prop07_df)
     
     # encode features
-    encode_features(prop_df)
-    
+    encode_features(prop06_df)
+    encode_features(prop07_df)
+   
     # add features
-    add_features(prop_df)
+    add_features(prop06_df)
+    add_features(prop07_df)
     
     # merge data
-    train_x = train_df.merge(prop_df, how='left', on='parcelid')
+    train06_x = train06_df.merge(prop06_df, how='left', on='parcelid')
+    train07_x = train07_df.merge(prop07_df, how='left', on='parcelid')
+    train_x = pd.concat([train06_x, train07_x], axis=0)
     
     # analyze (optional)
-    analyze(train_x)
-    
+    if False:
+        analyze(train_x)
+        
     # remove outliers
     remove_outliers(train_x)
-    
+        
     # select features
     select_features(train_x)
-
+    
     # prepare train and valid data
     print('\nPreparing train and valid data ...')
     
     train_y = train_x['logerror']
     train_x.drop(['logerror'], axis=1, inplace=True)
 
-    full_train_x = train_x.copy()
-    full_train_y = train_y.copy()
-    
-    select_qtr4 = pd.to_datetime(train_df["transactiondate"]).dt.month > 9
-    train_x, valid_x = train_x[~select_qtr4], train_x[select_qtr4]
-    train_y, valid_y = train_y[~select_qtr4], train_y[select_qtr4]
-
-    print('full train x shape: ', full_train_x.shape)
-    print('full train y shape: ', full_train_y.shape)    
+    if USE_VALID_DATA is True:
+        pass
+        #select_qtr4 = pd.to_datetime(train06_df["transactiondate"]).dt.month>9
+        #train06_x, valid06_x = train06_x[~select_qtr4], train06_x[select_qtr4]
+        #train06_y, valid06_y = train06_y[~select_qtr4], train06_y[select_qtr4]
+        
     print('train x shape: ', train_x.shape)
     print('train y shape: ', train_y.shape)
-    print('valid x shape: ', valid_x.shape)
-    print('valid y shape: ', valid_y.shape)
+    if valid_x is not None:
+        print('valid x shape: ', valid_x.shape)
+        print('valid y shape: ', valid_y.shape)
+    else:
+        print('valid x shape: None')
+        print('valid y shape: None')            
     
     # prepare test data
     print('\nPreparing test data ...')
     
-    test_x = prop_df[train_x.columns]
+    test_x = prop06_df[train_x.columns]
+        
     print('test x shape: ', test_x.shape)
-    
+        
     # release
-    del train_df
-    del prop_df
+    del train06_df
+    del prop06_df
+    del train07_df
+    del prop07_df    
     gc.collect()
     
 
@@ -254,7 +265,7 @@ def tune_eta(params):
     print('\nTuning eta ...')
     
     num_boost_round = 5000
-    d_train = xgb.DMatrix(full_train_x, label=full_train_y)
+    d_train = xgb.DMatrix(train_x, label=train_y)
     
     eta_list = [0.2, 0.1, 0.05, 0.025, 0.005, 0.0025]
     min_mae = float("Inf")
@@ -292,7 +303,7 @@ def tune_max_depth__min_child_weight(params):
     print('\nTuning max_depth and min_child_weight ...')
     
     num_boost_round = 5000
-    d_train = xgb.DMatrix(full_train_x, label=full_train_y)
+    d_train = xgb.DMatrix(train_x, label=train_y)
     
     max_depth_list = list(range(5,10))
     min_child_weight_list = list(range(1,5))
@@ -335,7 +346,7 @@ def tune_subsample__colsample_bytree(params):
     print('\nTuning subsample and colsample_bytree ...')
     
     num_boost_round = 5000
-    d_train = xgb.DMatrix(full_train_x, label=full_train_y)
+    d_train = xgb.DMatrix(train_x, label=train_y)
     
     subsample_list = [0.1, 0.2, 0.4, 0.6, 0.8, 1.0]
     colsample_bytree = [0.1, 0.2, 0.4, 0.6, 0.8, 1.0]
@@ -378,7 +389,7 @@ def tune_alpha_lambda(params):
     print('\nTuning alpha and lambda ...')
     
     num_boost_round = 5000
-    d_train = xgb.DMatrix(full_train_x, label=full_train_y)
+    d_train = xgb.DMatrix(train_x, label=train_y)
     
     alpha_list = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
     lambda_list = [1.0, 2.0, 4.0, 6.0, 8.0, 10.0]
@@ -510,9 +521,11 @@ def _build_model():
 def _train():
     print('\n\nSTEP3: training ...')
     
-    if TRAIN_FULL_SET is False:
+    global xgb_clf
+    global lgb_clf
+    
+    if USE_VALID_DATA is True:
         # xgboost
-        global xgb_clf
         d_train = xgb.DMatrix(train_x, label=train_y)
         d_valid = xgb.DMatrix(valid_x, label=valid_y)
         evals = [(d_train, 'train'), (d_valid, 'valid')]
@@ -521,7 +534,6 @@ def _train():
                             early_stopping_rounds=100, verbose_eval=10)
         
         # ligtgbm
-        global lgb_clf
         d_train = lgb.Dataset(train_x, label=train_y)
         d_valid = lgb.Dataset(valid_x, label=valid_y)
         valid_sets = [d_train, d_valid]
@@ -532,33 +544,32 @@ def _train():
                             early_stopping_rounds=100,verbose_eval=10)
     else:
         # xgboost
-        global full_xgb_clf
-        full_d_train = xgb.DMatrix(full_train_x, label=full_train_y)
-        evals = [(full_d_train, 'train')]
-        full_xgb_clf = xgb.train(full_xgb_params, full_d_train, 
-                                 num_boost_round=5000, 
-                                 evals=evals,verbose_eval=10)
+        d_train = xgb.DMatrix(train_x, label=train_y)
+        evals = [(d_train, 'train')]
+        xgb_clf = xgb.train(full_xgb_params, d_train, 
+                            num_boost_round=5000, 
+                            evals=evals,verbose_eval=10)
     
             
 ## STEP4: predict
 def _predict():
     print('\n\nSTEP4: predicting ...')
     
-    if TRAIN_FULL_SET is False:
+    global xgb_pred
+    global lgb_pred
+    
+    if USE_VALID_DATA is True:
         # xgboost
-        global xgb_pred
         d_test = xgb.DMatrix(test_x)
         xgb_pred = xgb_clf.predict(d_test)
     
         # lightgbm
-        global lgb_pred
         test_x.values.astype(np.float32, copy=False)
         lgb_pred = lgb_clf.predict(test_x)
     else:
         # xgboost
-        global full_xgb_pred
         d_test = xgb.DMatrix(test_x)
-        full_xgb_pred = full_xgb_clf.predict(d_test)
+        xgb_pred = xgb_clf.predict(d_test)
     
     
 ## STEP5: generate submission    
@@ -567,10 +578,10 @@ def _generate_submission():
 
     submission = pd.read_csv(submission_path)
     for c in submission.columns[submission.columns != 'ParcelId']:
-        if TRAIN_FULL_SET is False:
+        if USE_VALID_DATA is True:
             submission[c] = xgb_pred*XGB_WEIGHT + lgb_pred*LGB_WEIGHT
         else:
-            submission[c] = full_xgb_pred            
+            submission[c] = xgb_pred            
         
     submission.to_csv('sub{}.csv'.format(datetime.now().\
                 strftime('%Y%m%d_%H%M%S')), index=False, float_format='%.5f')
@@ -579,11 +590,13 @@ def _generate_submission():
 ## main
 def main():
     _process_data()
-    #_tune_params()
-    _build_model()
-    _train()
-    _predict()
-    _generate_submission()
+    if TUNE_PARAMS is True:
+        _tune_params()
+    else:
+        _build_model()
+        _train()
+        _predict()
+        _generate_submission()
     
 
 if __name__ == "__main__":
